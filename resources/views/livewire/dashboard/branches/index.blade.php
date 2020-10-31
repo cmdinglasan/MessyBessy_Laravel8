e282be17c2a6311d77f3f02b8264f35331e74802
cmdinglasan <cmdinglasan@student.apc.edu.ph>
2020-10-31 21:16:45 +0800

    Added Employee and Branches Livewire

---
+++ resources/views/livewire/dashboard/branches/index.blade.php
@@ -0,0 +1,22 @@
+<div class="db-card-header db-card-header-green">
+	<div class="container">
+		<div class="header-title">
+			Branches
+		</div>
+		<div class="header-subtitle">
+			Where are those branches at?
+		</div>
+		<div class="header-action">
+			<a href="#settings" class="btn header-btn">
+				Go to settings
+			</a>
+		</div>
+		<div class="header-image">
+		</div>
+	</div>
+</div>
+<div class="db-content">
+	<div class="container">
+		
+	</div>
+</div>
