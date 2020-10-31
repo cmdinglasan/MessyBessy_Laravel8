67fa1289f316c18984f3871bc7142924df4611c6
Rommel Ormita <ormita.rommel@outlook.com>
2020-11-01 00:46:30 +0800

    View Employees

--- app/Http/Livewire/Dashboard/Employees/Index.php
+++ app/Http/Livewire/Dashboard/Employees/Index.php
@@ -3,11 +3,39 @@
 namespace App\Http\Livewire\Dashboard\Employees;
 
 use Livewire\Component;
+use Livewire\WithPagination;
+use App\Models\User;
 
 class Index extends Component
 {
+    use WithPagination;
+	public $user,  $query, $results;
+
     public function render()
     {
-        return view('livewire.dashboard.employees.index');
+        $users = User::paginate(25);
+
+        return view('livewire.dashboard.employees.index', compact('users'));
+    }
+
+    protected $rules = [
+        'searchText' => 'required|min:1',
+        'users='=> 'nullable',
+    ];
+
+    public function updatedQuery()
+    {
+        $this->results = User::where('name','like', '%'.$this->query.'%')
+            ->orderBy('name','asc')
+            ->get()
+            ->toArray();
+    }
+
+    public function mount()
+    {
+        $this->query = '';
+        $this->users = User::orderBy('name','asc')
+            ->get()
+            ->toArray();
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -24,8 +24,11 @@ class User extends Authenticatable
      *
      * @var array
      */
+
+    protected $guarded = [];
+
     protected $fillable = [
-        'name', 'email', 'password',
+        'name', 'email', 'password', 'last_login_at'
     ];
 
     /**
