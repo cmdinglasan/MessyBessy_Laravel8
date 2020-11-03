0125d19e41dedbed2b29b9d53c85c7ba868dfe3a
ebrocamora <ebrocamora@student.apc.edu.ph>
2020-11-03 11:32:27 +0800

    fixed, still cannot add/edit/delete product

--- app/Http/Livewire/Dashboard/Branches/Index.php
+++ app/Http/Livewire/Dashboard/Branches/Index.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Livewire;
+namespace App\Http\Livewire\Dashboard\Branches;
 
 use Livewire\Component;
 use App\Models\Branch;
--- resources/views/livewire/dashboard/branches/index.blade.php
+++ resources/views/livewire/dashboard/branches/index.blade.php
@@ -38,7 +38,7 @@
 
             @endif
 
-            <button wire:click="create()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Create New Post</button>
+            <button wire:click="create()" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded my-3">Create New Branch</button>
 
             @if($isOpen)
 
