f58ac2f4eefff8d89c81dda2c1d576bd5c216f03
ebrocamora <ebrocamoragmail@gmail.com>
2020-11-03 12:32:29 +0800

    Fixed migration file foreign key error

--- database/migrations/2020_10_21_163311_create_stock_table.php
+++ database/migrations/2020_10_21_163311_create_stock_table.php
@@ -13,6 +13,40 @@ class CreateStockTable extends Migration
      */
     public function up()
     {
+        Schema::create('product_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->unsignedBigInteger('product_category_id');
+            $table->string('image')->nullable();
+            $table->unsignedDecimal('price', 10, 2);
+            $table->unsignedinteger('stock')->default(0);
+            $table->unsignedinteger('stock_defective')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('product_category_id')->references('id')->on('product_categories');
+        });
+        Schema::create('branches', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('branch_name');
+            $table->string('branch_street');
+            $table->string('branch_city');
+            $table->string('branch_state');
+            $table->integer('branch_post_code');
+            $table->string('branch_country');
+            $table->string('branch_contact_number');
+            $table->string('branch_operating_hours');
+            $table->string('branch_other_info')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+        });
         Schema::create('stocks', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->unsignedinteger('stock')->default(0);
@@ -33,6 +67,10 @@ class CreateStockTable extends Migration
      */
     public function down()
     {
+        Schema::dropIfExists('product_categories');
+        Schema::dropIfExists('products');
+        Schema::dropIfExists('branches');
         Schema::dropIfExists('stock');
+
     }
 }
