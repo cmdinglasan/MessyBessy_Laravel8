31f1aac9679245995a09ffc60848e445c2c35641
cmdinglasan <cmdinglasan@student.apc.edu.ph>
2020-11-19 02:42:41 +0800

    Fixed error in employee

--- resources/views/livewire/dashboard/employees/index.blade.php
+++ resources/views/livewire/dashboard/employees/index.blade.php
@@ -5,67 +5,67 @@
 		</div>
 		<div class="sidebar-search px-4 pb-2 pt-3">
 			<div class="form-group">
-				<input type="text" placeholder="Search account here..." wire:model="query" class="bg-gray-200 w-full p-3 outline-none" />
+				<input type="text" placeholder="Search account here..." wire:model="query" class="bg-gray-200 w-full p-3 outline-none"/>
 			</div>
 		</div>
 		<div class="sidebar-content relative h-auto scrolling-auto">
 			<div class="account-list">
 				@if(!empty($query))
-				@foreach($results as $user)
-				<a href="?userID={{ $user['id'] }}" class="p-4 hover:bg-gray-200 block flex hover:no-underline hover:text-gray-800">
-					<div class="account-photo w-8 mr-3 align-middle">
-						<img class="h-8 w-8 rounded-full object-cover" src="{{ $user['profile_photo_url'] }}" alt="{{ $user['name'] }}">
-					</div>
-					<div class="account-info flex-auto align-middle">
-						<h4 class="account-name font-bold mb-0">
-							{{ $user['name'] }}
-						</h4>
-						<span class="account-role text-xs -mt-1 capitalize">
-							@if(!empty($user['current_team_id']))
-							@foreach($roles as $role)
-							@if($user['id'] == $role->user_id)
-							{{ $role->user_id }}
-							@endif
-							@endforeach
-							@else
-							@foreach($roles as $role)
-							@if($user['id'] == $role->user_id)
-							{{ $role->role }}
-							@endif
-							@endforeach
-							@endif
-						</span>
-					</div>
-				</a></a>
-				@endforeach
+					@foreach($results as $user)
+						<a href="?userID={{ $user['id'] }}" class="p-4 hover:bg-gray-200 block flex hover:no-underline hover:text-gray-800">
+							<div class="account-photo w-8 mr-3 align-middle">
+								<img class="h-8 w-8 rounded-full object-cover" src="{{ $user['profile_photo_url'] }}" alt="{{ $user['name'] }}">
+							</div>
+							<div class="account-info flex-auto align-middle">
+								<h4 class="account-name font-bold mb-0">
+									{{ $user['name'] }}
+								</h4>
+								<span class="account-role text-xs -mt-1 capitalize">
+								@if(!empty($user['current_team_id']))
+									@foreach($roles as $role)
+										@if($user['id'] == $role->user_id)
+											{{ $role->user_id }}
+										@endif
+									@endforeach
+								@else
+									@foreach($roles as $role)
+										@if($user['id'] == $role->user_id)
+											{{ $role->role }}
+										@endif
+									@endforeach
+								@endif
+								</span>
+							</div>
+						</a></a>
+					@endforeach
 				@else
-				@foreach($users as $user)
-				<a href="?userID={{ $user['id'] }}" class="p-4 block hover:bg-gray-200 flex hover:no-underline hover:text-gray-800 @if($user['id'] == $userID) bg-gray-200 @else @endif">
-					<div class="account-photo w-8 mr-3 align-middle">
-						<img class="h-8 w-8 rounded-full object-cover" src="{{ $user['profile_photo_url'] }}" alt="{{ $user['name'] }}">
-					</div>
-					<div class="account-info flex-auto align-middle">
-						<h4 class="account-name font-bold mb-0">
-							{{ $user['name'] }}
-						</h4>
-						<span class="account-role text-xs -mt-1 capitalize">
-							@if(!empty($user['current_team_id']))
-							@foreach($roles as $role)
-							@if($user['id'] == $role->user_id)
-							{{ $role->user_id }}
-							@endif
-							@endforeach
-							@else
-							@foreach($roles as $role)
-							@if($user['id'] == $role->user_id)
-							{{ $role->role }}
-							@endif
-							@endforeach
-							@endif
-						</span>
-					</div>
-				</a></a>
-				@endforeach
+					@foreach($users as $user)
+						<a href="?userID={{ $user['id'] }}" class="p-4 block hover:bg-gray-200 flex hover:no-underline hover:text-gray-800 @if($user['id'] == $userID) bg-gray-200 @else @endif">
+							<div class="account-photo w-8 mr-3 align-middle">
+								<img class="h-8 w-8 rounded-full object-cover" src="{{ $user['profile_photo_url'] }}" alt="{{ $user['name'] }}">
+							</div>
+							<div class="account-info flex-auto align-middle">
+								<h4 class="account-name font-bold mb-0">
+									{{ $user['name'] }}
+								</h4>
+								<span class="account-role text-xs -mt-1 capitalize">
+								@if(!empty($user['current_team_id']))
+									@foreach($roles as $role)
+										@if($user['id'] == $role->user_id)
+											{{ $role->user_id }}
+										@endif
+									@endforeach
+								@else
+									@foreach($roles as $role)
+										@if($user['id'] == $role->user_id)
+											{{ $role->role }}
+										@endif
+									@endforeach
+								@endif
+								</span>
+							</div>
+						</a></a>
+					@endforeach
 				@endif
 			</div>
 		</div>
@@ -73,95 +73,89 @@
 	<div class="bg-white flex-auto m-5">
 		<div class="container">
 			@if(!empty($userID))
-			@foreach($userInfo as $user)
-			<div class="account">
-				<div class="account-details flex">
-					<div class="account-photo flex w-24 mr-10">
-						<img class="h-24 w-24 rounded-full object-cover" src="{{ $user['profile_photo_url'] }}" alt="{{ $user['name'] }}">
-					</div>
-					<div class="account-info flex-auto">
-						<h2 class="text-4xl font-black">
-							{{ $user['name'] }}
-						</h2>
-						<span class="capitalize font-bold block">
-							@if(!empty($user['current_team_id']))
-							@foreach($roles as $role)
-							@if($user['id'] == $role->user_id)
-							{{ $role->user_id }}
-							@endif
-							@endforeach
-							@else
-							@foreach($roles as $role)
-							@if($user['id'] == $role->user_id)
-							{{ $role->role }}
-							@endif
-							@endforeach
-							@endif
-						</span>
-						<span class="text-gray-500">
-							@foreach($teams as $team)
-							@if($user['id'] == $team['user_id'])
-							@if($team['personal_team'] == $team['id'])
-							{{ $team['name'] }}
-							@endif
-							@endif
-							@endforeach
-						</span>
-					</div>
-				</div>
-				<div class="account-overview my-10">
-
-					<button class="btn btn-primary" onclick="test();">Edit</button>
-
-					<button wire:click="update()" class="btn btn-primary">Save</button>
-					<div class="form">
-						<div class="form-group">
-							<label for="name" class="text-xs uppercase text-gray-400 font-bold">Name</label>
-							<input wire:model="name" type="text" id="name" class="form-control" readonly="" />
-							@if($errors->has('name'))
-							<p style="color: red">{{$errors->first('name')}}</p>
-							@endif
-						</div>
-						<div class="row">
-							<div class="form-group col">
-								<label for="email" class="text-xs uppercase text-gray-400 font-bold">Email Address</label>
-								<input wire:model="email" type="email" id="email" class="form-control" readonly="" />
-								@if($errors->has('email'))
-								<p style="color: red">{{$errors->first('email')}}</p>
-								@endif
+				@foreach($userInfo as $user)
+					<div class="account">
+						<div class="account-details flex">
+							<div class="account-photo flex w-24 mr-10">
+								<img class="h-24 w-24 rounded-full object-cover" src="{{ $user['profile_photo_url'] }}" alt="{{ $user['name'] }}">
 							</div>
-							<div class="form-group col">
-								<label for="email_verify" class="text-xs uppercase text-gray-400 font-bold">Verified?</label>
-								<input wire:model="email_verified_at" type="text" id="email_verify" class="form-control" disabled="" />
-								<!-- value="@if(!empty($user['email_verified_at'])) Yes @else No @endif" -->
+							<div class="account-info flex-auto">
+								<h2 class="text-4xl font-black">
+									{{ $user['name'] }}
+								</h2>
+								<span class="capitalize font-bold block">
+									@if(!empty($user['current_team_id']))
+										@foreach($roles as $role)
+											@if($user['id'] == $role->user_id)
+												{{ $role->user_id }}
+											@endif
+										@endforeach
+									@else
+										@foreach($roles as $role)
+											@if($user['id'] == $role->user_id)
+												{{ $role->role }}
+											@endif
+										@endforeach
+									@endif
+								</span>
+								<span class="text-gray-500">
+									@foreach($teams as $team)
+										@if($user['id'] == $team['user_id'])
+											@if($team['personal_team'] == $team['id'])
+												{{ $team['name'] }}
+											@endif
+										@endif
+									@endforeach
+								</span>
 							</div>
 						</div>
-						<!-- <div class="form-group">
-							<label for="password" class="text-xs uppercase text-gray-400 font-bold">Password</label>
-							<input wire:model="password" type="text" id="password" value="Hidden" class="form-control" disabled="" />
-						</div> -->
+						<div class="account-overview my-10">
+
+						<button class="btn btn-primary" onclick="test();">Edit</button>
+
+						<button wire:click="update()" class="btn btn-primary">Save</button>
+						
+							<div class="form">
+								<div class="form-group">
+									<label for="name" class="text-xs uppercase text-gray-400 font-bold">Name</label>
+									<input type="text" id="name" value="{{ $user['name'] }}" class="form-control" readonly="" />
+								</div>
+								<div class="row">
+									<div class="form-group col">
+										<label for="email" class="text-xs uppercase text-gray-400 font-bold">Email Address</label>
+										<input type="email" id="email" value="{{ $user['email'] }}" class="form-control" readonly="" />
+									</div>
+									<div class="form-group col">
+										<label for="email_verify" class="text-xs uppercase text-gray-400 font-bold">Verified?</label>
+										<input type="text" id="email_verify" value="@if(!empty($user['email_verified_at'])) Yes @else No @endif" class="form-control" disabled="" />
+									</div>
+								</div>
+								<div class="form-group">
+									<label for="password" class="text-xs uppercase text-gray-400 font-bold">Password</label>
+									<input type="text" value="Hidden" class="form-control" disabled="" />
+								</div>
+							</div>
+						</div>
+						<div class="account-activity">
+							<div class="activity-header my-3">
+								<h4 class="text-xl font-bold">Activity Log</h4>
+							</div>
+							<div class="activity-log">
+								<span>To be implemented</span>
+							</div>
+						</div>
 					</div>
-				</div>
-				<div class="account-activity">
-					<div class="activity-header my-3">
-						<h4 class="text-xl font-bold">Activity Log</h4>
-					</div>
-					<div class="activity-log">
-						<span>To be implemented</span>
-					</div>
-				</div>
-			</div>
-			@endforeach
+				@endforeach
 			@else
-			<div class="account">
-				<div class="account-details flex">
-					<div class="account-info flex-auto">
-						<h2 class="text-4xl font-black">
-							Select an account
-						</h2>
+				<div class="account">
+					<div class="account-details flex">
+						<div class="account-info flex-auto">
+							<h2 class="text-4xl font-black">
+								Select an account
+							</h2>
+						</div>
 					</div>
 				</div>
-			</div>
 			@endif
 		</div>
 	</div>
